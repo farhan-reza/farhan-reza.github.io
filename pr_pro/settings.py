bebe919541a6ba7a88b91e9aa71ab1549e5155d4
Farhan Reza <Farhan Reza>
2023-05-01 14:19:48 +0530

    d

--- pr_pro/settings.py
+++ pr_pro/settings.py
@@ -130,6 +130,10 @@ STATIC_ROOT = BASE_DIR / 'productionFiles'
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'myStaticFiles',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
